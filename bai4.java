5fa621a77d15508f2bd72761e258cf0227d513d3
Your Name <you@example.com>
2021-11-08 19:41:20 +0700

    fix bai 4

--- bai4.java
+++ bai4.java
@@ -4,7 +4,7 @@ public class bai4 {
     public static void main(String[] args) {
         float soDuTaiKhoan;
         float tienThanhToanTrongThang;
-        float tienPhat ;
+        float tienPhat =0 ;
         final float lai = 1.5f;
         float soTienChuaThanhToan;
         Scanner sc = new Scanner(System.in);
@@ -16,11 +16,11 @@ public class bai4 {
 
         if (soTienChuaThanhToan > 0) {
             tienPhat = soTienChuaThanhToan * lai / 100;
-            System.out.println("so tien phat la "+tienPhat);
+            
         }else{
             System.out.println("ko co tien phat");
         }
-        
+       System.out.println("tien phat la"+tienPhat);
         
        //aaaaaaa
         sc.close();
